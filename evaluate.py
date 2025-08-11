7f0275050c2f1e0a031c991da80d2847f8763d69
maplesyrup-0606 <mercurymcindoe@gmail.com>
2025-08-11 19:25:29 -0400

    NSERC: local modifications

--- evaluate.py
+++ evaluate.py
@@ -1,62 +1,72 @@
 import json
+from pathlib import Path
 
-ans_file = ''
-label_file = ''
+# ans_file = ''
+# label_file = ''
 
-answers = [json.loads(q) for q in open(ans_file, 'r')]
-label_list = [json.loads(q)['label'] for q in open(label_file, 'r')]
+# ans_file = Path("~/NSERC/data/generated_captions/jul30_samples/generated_captions/POPE/MSCOCO2014/baseline_answers.jsonl").expanduser()
+# ans_file = Path("~/NSERC/data/generated_captions/jul30_samples/generated_captions/POPE/MSCOCO2014/baseline-13b_answers.jsonl").expanduser()
+ans_root = Path("~/NSERC/data/generated_captions/jul30_samples/generated_captions/POPE/MSCOCO2014").expanduser()
+label_file = Path("~/NSERC/eval/captions/hallucination/POPE/output/coco/coco_pope_random.jsonl").expanduser()
+for ans_file in ans_root.glob("*_answers.jsonl") :
 
-for answer in answers:
-    text = answer['answer']
+    answers = [json.loads(q) for q in open(ans_file, 'r')]
+    label_list = [json.loads(q)['label'] for q in open(label_file, 'r')]
+    
+    Total = len(answers)
 
-    # Only keep the first sentence
-    if text.find('.') != -1:
-        text = text.split('.')[0]
+    for answer in answers:
+        text = answer['answer']
 
-    text = text.replace(',', '')
-    words = text.split(' ')
-    if 'No' in words or 'not' in words or 'no' in words:
-        answer['answer'] = 'no'
-    else:
-        answer['answer'] = 'yes'
+        # Only keep the first sentence
+        if text.find('.') != -1:
+            text = text.split('.')[0]
 
-for i in range(len(label_list)):
-    if label_list[i] == 'no':
-        label_list[i] = 0
-    else:
-        label_list[i] = 1
+        text = text.replace(',', '')
+        words = text.split(' ')
+        if 'No' in words or 'not' in words or 'no' in words:
+            answer['answer'] = 'no'
+        else:
+            answer['answer'] = 'yes'
 
-pred_list = []
-for answer in answers:
-    if answer['answer'] == 'no':
-        pred_list.append(0)
-    else:
-        pred_list.append(1)
+    for i in range(len(label_list)):
+        if label_list[i] == 'no':
+            label_list[i] = 0
+        else:
+            label_list[i] = 1
 
-pos = 1
-neg = 0
-yes_ratio = pred_list.count(1) / len(pred_list)
+    pred_list = []
+    for answer in answers:
+        if answer['answer'] == 'no':
+            pred_list.append(0)
+        else:
+            pred_list.append(1)
 
-TP, TN, FP, FN = 0, 0, 0, 0
-for pred, label in zip(pred_list, label_list):
-    if pred == pos and label == pos:
-        TP += 1
-    elif pred == pos and label == neg:
-        FP += 1
-    elif pred == neg and label == neg:
-        TN += 1
-    elif pred == neg and label == pos:
-        FN += 1
+    pos = 1
+    neg = 0
+    yes_ratio = pred_list.count(1) / len(pred_list)
 
-print('TP\tFP\tTN\tFN\t')
-print('{}\t{}\t{}\t{}'.format(TP, FP, TN, FN))
+    TP, TN, FP, FN = 0, 0, 0, 0
+    for pred, label in zip(pred_list, label_list):
+        if pred == pos and label == pos:
+            TP += 1
+        elif pred == pos and label == neg:
+            FP += 1
+        elif pred == neg and label == neg:
+            TN += 1
+        elif pred == neg and label == pos:
+            FN += 1
 
-precision = float(TP) / float(TP + FP)
-recall = float(TP) / float(TP + FN)
-f1 = 2*precision*recall / (precision + recall)
-acc = (TP + TN) / (TP + TN + FP + FN)
-print('Accuracy: {}'.format(acc))
-print('Precision: {}'.format(precision))
-print('Recall: {}'.format(recall))
-print('F1 score: {}'.format(f1))
-print('Yes ratio: {}'.format(yes_ratio))
+    print(f"Current: {ans_file.stem}\tTotal: {Total}")
+    print('TP\tFP\tTN\tFN\t')
+    print('{}\t{}\t{}\t{}'.format(TP, FP, TN, FN))
+
+    precision = float(TP) / float(TP + FP)
+    recall = float(TP) / float(TP + FN)
+    f1 = 2*precision*recall / (precision + recall)
+    acc = (TP + TN) / (TP + TN + FP + FN)
+    print('Accuracy: {}'.format(acc))
+    print('Precision: {}'.format(precision))
+    print('Recall: {}'.format(recall))
+    print('F1 score: {}'.format(f1))
+    print('Yes ratio: {}'.format(yes_ratio))
